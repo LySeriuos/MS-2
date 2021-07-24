5c26429be198ccc701612f9fae0ae3301655a9aa
LySeriuos <drungilas.p@gmail.com>
2021-07-24 08:47:09 +0000

    created an array for random sequence in the game

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// arrenging query solectors for colour sectors 
+const topLeft = document.querySelector('.top-left-sector');
+const topRight = document.querySelector('.top-right-sector');
+const bottomLeft = document.querySelector('.bottom-left-sector');
+const bottomRight = document.querySelector('.top-right-sector');
