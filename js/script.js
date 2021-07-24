245ace154590eaec062a074ec2d3f0d4284d939e
LySeriuos <drungilas.p@gmail.com>
2021-07-24 09:24:05 +0000

    addedd json

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // arrenging query solectors for colour sectors 
-const topLeft = document.querySelector('.top-left-sector');
-const topRight = document.querySelector('.top-right-sector');
-const bottomLeft = document.querySelector('.bottom-left-sector');
-const bottomRight = document.querySelector('.top-right-sector');
+let topLeft = document.querySelector('.top-left-sector');
+let topRight = document.querySelector('.top-right-sector');
+let bottomLeft = document.querySelector('.bottom-left-sector');
+let bottomRight = document.querySelector('.top-right-sector');
+
+//creating a array to give random sequence in the game
+
+let sequences = [topLeft, bottomLeft, bottomRight, topRight];
+
+let flash = (sector) {
+return new Promise((resolve, reject) {
+   sector.className += 'active';
+   setTimeout(() {
+sector.className = sector.className.replace('active', '');
+resolve();
+   }, 500)
+});
+};
